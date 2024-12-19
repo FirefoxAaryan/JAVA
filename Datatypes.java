992c8acc9207a9c511cdc3bf8d88188f1dc3474e
Aaryan Bose <aaryanbose22@gmail.com>
2024-12-19 11:47:51 +0530

    ok

---
+++ Datatypes.java
@@ -0,0 +1,8 @@
+public class Datatypes {
+    public static void main(String[] args){
+        int a = 64;
+        float b = 98.76f;
+        double bigfloat = 98.1234556;
+        long integer = 1234567890l;
+    }
+}

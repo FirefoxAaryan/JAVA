86b92f788516230d9bf264d444652624a41ba335
Aaryan Bose <aaryanbose22@gmail.com>
2024-12-19 12:06:06 +0530

    1

--- Input.java
+++ Input.java
@@ -1,9 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
+import java.util.Scanner;
 
 class Input {
-
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your Marks : ");
+        int marks = input.nextInt();
+        System.out.println("Your marks are : " + marks);
+    }
 }
+

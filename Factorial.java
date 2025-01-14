af0bff33e4716aec64e3af37f359a83805e3d199
Aaryan Bose <aaryanbose22@gmail.com>
2025-01-14 13:32:06 +0530

    Factorial

---
+++ Factorial.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Factorial{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num;
+
+        System.out.print("Enter the number for factorial : ");
+        num = sc.nextInt();
+
+        int fact = Factorial(num);
+        System.out.println("Factorial of " + num + " = " + fact);
+        }
+
+    public static int Factorial(int num){
+        int factorial = 1;
+
+        if(num >= 0){
+            for(int i = num; i > 0; i--){
+                factorial *= i;
+            }
+        }
+        else{
+            System.out.println("There are no factorials for negative numbers !");
+            return 0;
+        }
+    return factorial;
+    }
+}
